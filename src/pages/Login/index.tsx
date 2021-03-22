7be18caf46ca6157052e3fdf1f9420a1d901fc04
soluteli <soluteli@163.com>
2021-03-22 19:26:19 +0800

    chore: add fusion/design

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Button } from '@alifd/next';
+
+export default function LoginAPP() {
+  return (
+    <>
+      <Button>我是按钮</Button>
+      <Button type="primary">我是按钮</Button>
+    </>
+  );
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import Home from '@/pages/Home';
 import EmotionPage from '@/pages/Emotion';
 import DemoGrid from '@/pages/DemoGrid';
+import LoginPage from '@/pages/Login';
 
 const routerConfig = [
   {
     path: '/',
     component: Home,
     exact: true,
+  },
+  {
+    path: '/login',
+    component: LoginPage,
   },
   {
     path: '/emotion',
